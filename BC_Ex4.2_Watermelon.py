6411615588cfa2ef60467b4089b01d0ff3be6b4b
n1ck <kulikov.91@gmail.com>
2026-06-06 17:35:09 +0300

    Add files via upload

---
+++ BC_Ex4.2_Watermelon.py
@@ -0,0 +1,7 @@
+w = int(input())
+if w % 2 == 0 and 2 < w <= 100:
+    print('YES')
+else:
+    print('NO')
+
+# Последние два теста провалил, потому что в input писал ('Введите вес арбуза: ') facepalm
